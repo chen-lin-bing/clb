3a494bf685fab809d51a705b974544abfcb6e0db
localhost <chenlinibng0@163.com>
2018-10-15 00:51:05 +0800

    auto

--- webhook.php
+++ webhook.php
@@ -1,22 +1,49 @@
-<?php
-// GitHub Webhook Secret.
-// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
-$secret = "";
-// Path to your respostory on your server.
-// e.g. "/var/www/respostory"
-$path = "";
-// Headers deliveried from GitHub
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-if ($signature) {
-  $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
+ <?php
 
-  file_put_contents('date.log,date('Y-m-d h:i:s'),FILE_APPEND');
-  if (strcmp($signature, $hash) == 0) {
-   // echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-  file_put_contents('date.log,date('Y-m-d h:i:s'),FILE_APPEND');
-   // echo 111;   
- exit();
-  }
-}
-http_response_code(404);
 
+    $target = '/www/clb'; // 生产环境web目录
+    //密钥
+    $secret = "";
+    $wwwUser = 'root';
+    $wwwGroup = 'root';
+
+    //日志文件地址
+   $fs = fopen('.date.log', 'a');
+
+    //获取GitHub发送的内容 
+    $json = file_get_contents('php://input');
+    $content = json_decode($json, true);
+    //github发送过来的签名  
+    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+    if (!$signature) {
+       fclose($fs);
+       return http_response_code(404);
+     }
+
+    list($algo, $hash) = explode('=', $signature, 2);
+    //计算签名  
+    $payloadHash = hash_hmac($algo, $json, $secret);
+
+    // 判断签名是否匹配  
+    if ($hash === $payloadHash) {
+        $cmd = "cd $target && git pull";
+        $res = shell_exec($cmd);
+
+        $res_log .= 'Success:'.PHP_EOL;
+        $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
+        $res_log .= $res.PHP_EOL;
+        $res_log .= '======================================================================='.PHP_EOL;
+
+        fwrite($fs, $res_log);
+        $fs and fclose($fs);
+
+
+      } else {
+            $res_log  = 'Error:'.PHP_EOL;
+            $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '>个commit：' . PHP_EOL;
+            $res_log .= '密钥不正确不能pull'.PHP_EOL;
+            $res_log .= '======================================================================='.PHP_EOL;
+           fwrite($fs, $res_log);
+           $fs and fclose($fs);
+      }
