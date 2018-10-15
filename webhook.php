ae1a3d9ced177b99c3972e04887954fe81adad9e
ailiyun <chenlinbing0@163.com>
2018-10-15 11:29:56 +0800

    auto

--- webhook.php
+++ webhook.php
@@ -12,9 +12,9 @@ if ($signature) {
   if (strcmp($signature, $hash) == 0) {
   // echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
  // echo '222';
- $file = 'date.log';
-  $a = file_put_contents('date.log','123');
- // exec($shell,);
+ $file = 'echo  123 > date.log';
+ // $a = file_put_contents('date.log','123');
+ exec($shell,'',$a);
 	var_dump($a);  
  	 exit();
   }
