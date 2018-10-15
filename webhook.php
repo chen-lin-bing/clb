63d92fc2a51e68c2d7aa31b0ea3339ced204e94b
ailiyun <chenlinbing0@163.com>
2018-10-15 11:04:07 +0800

    auto

--- webhook.php
+++ webhook.php
@@ -1,58 +1,18 @@
- <?php
-    $target = '/www/clb'; // 生产环境web目录
-    //密钥
-    $secret = "";
-    $wwwUser = 'root';
-    $wwwGroup = 'root';
-//  echo 1;
-    //日志文件地址
-   $fs = fopen('./date.log', 'a');
-
-    //获取GitHub发送的内容 
-    $json = file_get_contents('php://input');
-    $content = json_decode($json, true);
-    //github发送过来的签名  
-    $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-//	var_dump($content);
-  //  if (!$signature) {
-    //   fclose($fs);
-     //  return http_response_code(404);
-   //  }
-//$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-// exit();
+<?php
+// GitHub Webhook Secret.
+// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
+$secret = "";
+// Path to your respostory on your server.
+// e.g. "/var/www/respostory"
+$path = "";
+// Headers deliveried from GitHub
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
-//	echo 1;
-file_put_contetns('date1.log','123');
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
-  //  echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-   // exit();
-      //	echo 2;
-       // $cmd = "cd $target && git pull";
-       // $res = shell_exec($cmd);
-       // echo 1;
-	$res_log = '';
-        $res_log .= 'Success:'.PHP_EOL;
-        $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：' . PHP_EOL;
-        $res_log .= $res.PHP_EOL;
-        $res_log .= '======================================================================='.PHP_EOL;
-       echo $res_log;
-        fwrite($fs, $res_log);
-        $fs and fclose($fs);
-
-
-      } else {
-            $res_log  = 'Error:'.PHP_EOL;
-            $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '>个commit：' . PHP_EOL;
-            $res_log .= '密钥不正确不能pull'.PHP_EOL;
-            $res_log .= '======================================================================='.PHP_EOL;
-           fwrite($fs, $res_log);
-        //   $fs and fclose($fs);
-      
+    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+    exit();
   }
 }
- //   list($algo, $hash) = explode('=', $signature, 2);
-    //计算签名  
-   // $payloadHash = hash_hmac($algo, $json, $secret);
+http_response_code(404);
 
-    // 判断签名是否匹配  
