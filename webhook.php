5fd2b03f727b05209fc749a76b43fdaaf8dcfaa8
ailiyun <chenlinbing0@163.com>
2018-10-15 11:00:06 +0800

    auto

--- webhook.php
+++ webhook.php
@@ -19,10 +19,10 @@
      //  return http_response_code(404);
    //  }
 //$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-file_put_contetns('date.log','123');
-exit();
+// exit();
 if ($signature) {
-	echo 1;
+//	echo 1;
+file_put_contetns('date.log','123');
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
   //  echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
