3c45827d15aea70860c579fefe27563a294505d0
ailiyun <chenlinbing0@163.com>
2018-10-15 11:16:42 +0800

    auto

--- webhook.php
+++ webhook.php
@@ -10,9 +10,10 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
-  //  echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+  // echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
  // echo '222';
-  file_put_contents('date.log','123');  
+ $a = file_put_contents('date.log','123');
+	var_dump($a);  
   exit();
   }
 }
